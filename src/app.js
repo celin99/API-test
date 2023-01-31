ce61db5e5d6da389fd4148ae070522368457a8eb
Marcelo Santos <mhps012@gmail.com>
2023-01-31 17:02:00 -0300

    atualização

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+const Post = require('./models/Posts')
+
 app.use(express.json())
 
 app.get('/hello_world', (req, res) => {
@@ -9,7 +11,26 @@ app.get('/hello_world', (req, res) => {
 
 app.post('/create', (req, res) =>{
     const title = req.body.title
-    res.send(`Título: ${title}`)
+    res.send(`Post: ${title}`)
+})
+
+app.post('/create_post', async (req, res) => {
+    try {
+        const { title, content } = req.body
+        const post = await Post.create({ title, content })
+        res.send(post)
+    }catch(err) {
+        res.status(400).send(err)
+    }
+})
+
+app.get('/list_posts', async (req, res) => {
+    try {
+        const posts = await Post.find()
+        res.send({ posts })
+    }catch(err) {
+        res.status(400).send(err)
+    }
 })
 
 app.listen(5000, () => {
--- src/db/db.js
+++ src/db/db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
-
-mongoose.connect('mongodb://localhost/adc-api', {
-    useCreateIndex: true
+mongoose.set('strictQuery', false)
+mongoose.connect('mongodb://127.0.0.1/adc-api', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 }).then(() => {
     console.log('Mongo Connected')
 }).catch(err => {
